5925137200d6afb344ae89cf3b2c06f5dcd8283d
ankit mishra <ankit.mishra@theredpandas.com>
2017-11-04 19:29:44 +0530

    modified gitignore

--- app/index.js
+++ app/index.js
@@ -12,12 +12,12 @@ import {
 
 const log = debug('application:bootstrap')
 
-log('creating application dwdsade')
+// log('creating application dwdsade')
 const applicationNode = document.createElement('div')
 applicationNode.className = 'container'
 applicationNode.id = 'application'
 
-log('adding application node to body')
+// log('adding application node to body')
 document.body.appendChild(applicationNode)
 const add = function(e){
 items.push(e.target.value)
